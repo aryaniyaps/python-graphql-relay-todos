c1561614c94580ed92f6599dc15e43b73b1a1f94
Aryan Iyappan <aryaniyappan2006@gmail.com>
2024-06-27 18:48:52 +0530

    add coverage

--- server/app/todos/query.py
+++ server/app/todos/query.py
@@ -27,16 +27,20 @@ class TodoQuery:
         paginated_result = await todo_service.get_all(
             first=first,
             last=last,
-            after=int(
-                relay.from_base64(after)[1],
-            )
-            if after
-            else None,
-            before=int(
-                relay.from_base64(before)[1],
-            )
-            if before
-            else None,
+            after=(
+                int(
+                    relay.from_base64(after)[1],
+                )
+                if after
+                else None
+            ),
+            before=(
+                int(
+                    relay.from_base64(before)[1],
+                )
+                if before
+                else None
+            ),
         )
 
         return TodoConnectionType.from_paginated_result(
--- server/migrations/env.py
+++ server/migrations/env.py
@@ -19,8 +19,6 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = database_metadata
 
 config.set_main_option("sqlalchemy.url", str(settings.database_url))
