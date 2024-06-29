e374783d72a38be555637a274971257e13a583c9
Aryan Iyappan <aryaniyappan2006@gmail.com>
2024-06-29 11:28:39 +0530

    change loading screen

--- client/src/components/loader.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loader() {
-  return (
-    <div className="w-full h-full flex justify-center items-center">
-      loading...
-    </div>
-  );
-}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { RelayEnvironmentProvider } from "react-relay/hooks";
 import { RouterProvider } from "react-router-dom";
-import Loader from "./components/loader.tsx";
+import LoadingScreen from "./components/LoadingScreen.tsx";
 import "./index.css";
 import environment from "./lib/relay-environment";
 import router from "./router.tsx";
@@ -10,7 +10,7 @@ import router from "./router.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RelayEnvironmentProvider environment={environment}>
-      <Suspense fallback={<Loader />}>
+      <Suspense fallback={<LoadingScreen />}>
         <RouterProvider router={router} />
       </Suspense>
     </RelayEnvironmentProvider>
